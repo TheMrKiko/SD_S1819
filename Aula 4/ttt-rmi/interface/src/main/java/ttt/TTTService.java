f0c9fc9d990d5099d1d918e71e47a7fd3c09ebfe
Francisco Sousa <f.sou@hotmail.com>
2019-03-17 16:16:42 +0000

    Prepara aula 4: adiciona ficheiros dados

---
+++ Aula 4/ttt-rmi/interface/src/main/java/ttt/TTTService.java
@@ -0,0 +1,11 @@
+package ttt;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/** Tic Tac Toe game remote interface. */
+public interface TTTService extends Remote {
+    
+    /* TO-DO */
+
+}
