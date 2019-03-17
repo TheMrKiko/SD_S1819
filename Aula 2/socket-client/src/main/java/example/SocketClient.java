0c63c585c4320da700f26e54020d4221a900aea8
João Barata <jbarata1998@gmail.com>
2019-03-17 16:10:46 +0000

    Faz alteraçoes ao cliente

--- Aula 2/socket-client/src/main/java/example/SocketClient.java
+++ Aula 2/socket-client/src/main/java/example/SocketClient.java
@@ -1,8 +1,11 @@
 package example;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
+import java.util.Date;
 
 
 public class SocketClient {
@@ -20,12 +23,14 @@ public class SocketClient {
         int port = Integer.parseInt(args[1]);
         // Concatenate arguments using a string builder
         StringBuilder sb = new StringBuilder();
-        for (int i = 2; i < args.length; i++) {
-            sb.append(args[i]);
-            if (i < args.length-1) {
-                sb.append(" ");
-            }
-        }
+
+        String sd = "SD2019\n";
+        sb.append(sd);
+        sb.append(args[2]+"\n");
+
+        Date a = new Date();
+        sb.append(a.toString());
+        sb.append("\n");
         String text = sb.toString();
 
         // Create client socket
@@ -39,6 +44,19 @@ public class SocketClient {
         out.writeBytes(text);
         out.writeBytes("\n");
         System.out.println("Sent text: " + text);
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        String response2;
+        while (true) {
+            //Reads a line of text.
+            //A line ends with a line feed ('\n').
+            response2 = in.readLine();
+            if(response2.isEmpty()) {
+                break;
+            }
+            System.out.printf("Received message with content: '%s'%n", response2);
+        }
+
 
         // Close client socket
         socket.close();
